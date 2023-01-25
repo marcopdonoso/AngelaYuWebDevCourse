59c23c1a13da376d1458ee6436cb837ce524f289
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-01-25 10:19:54 -0400

    Adding background images to buttons

---
+++ Web Development/Drum Kit Starting Files/index.js
@@ -0,0 +1,10 @@
+var buttons = document.querySelectorAll(".drum");
+var audio  = new Audio("./sounds/tom-1.mp3");
+
+for (let index = 0; index < buttons.length; index++) {
+    buttons[index].addEventListener("click", function () {
+        audio.play();
+    });
+    
+}
+
