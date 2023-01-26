1ccb56d594c588c935358a9f27439ae3dfdf0be7
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-01-26 10:50:55 -0400

    Testing Event Listener

--- Web Development/Drum Kit Starting Files/index.js
+++ Web Development/Drum Kit Starting Files/index.js
@@ -1,9 +1,10 @@
 var buttons = document.querySelectorAll(".drum");
-var audio  = new Audio("./sounds/tom-1.mp3");
+//var audio  = new Audio("./sounds/tom-1.mp3");
 
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", function () {
-        audio.play();
+        //audio.play();
+        this.style.color = "white";
     });
     
 }
