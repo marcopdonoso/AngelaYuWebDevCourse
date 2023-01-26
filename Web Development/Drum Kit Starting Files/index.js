91bd7e38d9a7ba37c56d52357c429710c7531c2b
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-01-26 14:31:16 -0400

    Detecting clicks and keyboard pressing to play sounds

--- Web Development/Drum Kit Starting Files/index.js
+++ Web Development/Drum Kit Starting Files/index.js
@@ -1,11 +1,61 @@
+// Detecting button click
 var buttons = document.querySelectorAll(".drum");
-//var audio  = new Audio("./sounds/tom-1.mp3");
 
 for (let index = 0; index < buttons.length; index++) {
     buttons[index].addEventListener("click", function () {
-        //audio.play();
-        this.style.color = "white";
+        var key = this.innerHTML;
+        makeSound(key);
     });
     
 }
 
+// Detecting keyboard press
+
+document.addEventListener("keydown", function (event) {
+    var key = event.key;
+    makeSound(key);
+});
+
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var sound = new Audio("./sounds/crash.mp3");
+            sound.play();
+            break;
+
+        case "a":
+            var sound = new Audio("./sounds/kick-bass.mp3");
+            sound.play();
+            break;
+
+        case "s":
+            var sound = new Audio("./sounds/snare.mp3");
+            sound.play();
+            break;
+
+        case "d":
+            var sound = new Audio("./sounds/tom-1.mp3");
+            sound.play();
+            break;
+
+        case "j":
+            var sound = new Audio("./sounds/tom-2.mp3");
+            sound.play();
+            break;
+
+        case "k":
+            var sound = new Audio("./sounds/tom-3.mp3");
+            sound.play();
+            break;
+
+        case "l":
+            var sound = new Audio("./sounds/tom-4.mp3");
+            sound.play();
+            break;
+
+        default:
+            break;
+    }
+}
