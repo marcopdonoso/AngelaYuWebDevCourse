f4fcf385e0bb8d1ceee44869fee1be5e22898a05
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-02 13:12:33 -0400

    Adding more routes to response in case of GET messages to those routes from the browser

--- Web Development/my-express-server/server.js
+++ Web Development/my-express-server/server.js
@@ -6,6 +6,14 @@ app.get("/", function(req, res) {
     res.send("<h1>Hello World!</h1>");
 });
 
+app.get("/contact", function (req,res) {
+    res.send("Contact me at: marcopdonoso@gmail.com");
+});
+
+app.get("/about", function(req, res) {
+    res.send("I'm an electronics engineer learning Web Development at the age of 40, because I want to improve my incomes by doing something I love. Cheers!");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 });
