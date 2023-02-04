704d27f81a7fe0fbf0d94df8a306bef4bbd4b5ff
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-04 17:15:55 -0400

    Copying a Singin page from bootstrap and editing for my project

--- Web Development/Newsletter-Signup/app.js
+++ Web Development/Newsletter-Signup/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
@@ -8,4 +9,4 @@ const app = express();
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000.")
-})
+});
