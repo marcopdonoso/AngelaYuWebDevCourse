5631a2a3e760faf09bb640f4546c3867594dc3da
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-04 13:14:05 -0400

    Using Body Parser to Parse POST Request to the Server

--- Web Development/WeatherProject/app.js
+++ Web Development/WeatherProject/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(bodyParser.urlencoded({extended: true}));
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Cochabamba&units=metric&appid=3c3d658decb7027bc7ae6670e7956712";
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+    const query = req.body.cityName;
+    const apiKey = "3c3d658decb7027bc7ae6670e7956712";
+    const units = "metric";
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units="+ units +"&appid=" + apiKey;
 
     https.get(url, function(response) {
         console.log(response.statusCode);
@@ -18,7 +27,7 @@ app.get("/", function(req, res) {
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@4x.png";
 
             res.write("<p>The weather is currently " + description + "</p>");
-            res.write("<h1>The temperature in Cochabamba is " + temp + 
+            res.write("<h1>The temperature in " + query + " is " + temp + 
             " degrees Celcius</h1>");
             res.write("<img src=" + imageURL + ">");
             res.send();
