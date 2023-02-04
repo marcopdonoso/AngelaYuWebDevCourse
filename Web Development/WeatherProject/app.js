3306112b821a2e37d77367138ab62dda9e1ac8c9
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-04 12:33:25 -0400

    Rendering the web site with live API data using Express

--- Web Development/WeatherProject/app.js
+++ Web Development/WeatherProject/app.js
@@ -14,12 +14,16 @@ app.get("/", function(req, res) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            console.log(temp);
-            console.log(description);
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@4x.png";
+
+            res.write("<p>The weather is currently " + description + "</p>");
+            res.write("<h1>The temperature in Cochabamba is " + temp + 
+            " degrees Celcius</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
         });
     });
-
-    res.send("Server is up and running");
 });
 
 
