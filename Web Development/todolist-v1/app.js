7d8eb04c0ec2a7b48ec01bd3fc8a479cee0b84b8
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-09 16:24:09 -0400

    Creating the first EJS Template

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -4,15 +4,39 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res) {
     var today = new Date();
     var currentDay = today.getDay();
+    var day = "";
 
-    if (currentDay === 6 || currentDay === 0) {
-        res.write("<h1>Yay it's the weekend!</h1>");
-    } else {
-        res.sendFile(__dirname + "/index.html");
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wendsday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            break;
     }
+    res.render("list", {kindOfDay: day});
 });
 
 
