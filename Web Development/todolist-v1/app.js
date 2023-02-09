353bd465c68cc630ed15041adc38faedb05be5bb
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-09 15:56:39 -0400

    Creating HTML content

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -5,7 +5,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", function(req, res) {
-    res.send("Hello");
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0) {
+        res.write("<h1>Yay it's the weekend!</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+    }
 });
 
 
