cdb7ef38870367fe75f686ffbcc03751edd374e7
Marco Perez Donoso <marcopdonoso@gmail.com>
2023-02-10 11:30:23 -0400

    Passing Data from the web page to server

--- Web Development/todolist-v1/app.js
+++ Web Development/todolist-v1/app.js
@@ -4,41 +4,34 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wendsday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            break;
-    }
-    res.render("list", {kindOfDay: day});
+    var day = today.toLocaleDateString("en-US", options);
+
+
+    res.render("list", {kindOfDay: day, itemsList: items});
 });
 
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+
+    items.push(item);
+
+    res.redirect("/");
+});
 
 app.listen(PORT, function() {
     console.log("Server started and listening on port " + PORT);
